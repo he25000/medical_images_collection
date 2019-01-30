4e69921a5d616aba8c122057ed9063f346cf2ca1
hector25 <he25000@gmail.com>
2019-01-29 21:48:31 -0500

    added 3 function signature

--- preprocess_images/list_directory.py
+++ preprocess_images/list_directory.py
@@ -4,7 +4,9 @@ class images_collections:
 	paths = []
 	def __init__(self,d_path):
 		self.path=d_path
-
+        #def load_png_images
+	#def resize_images (target size, target dir)
+	#def  get_features(directory for features)
 	def get_list_files_directory(self, dir_path):
 		paths=[]
 		for root, dirs, files in os.walk(dir_path):
@@ -12,6 +14,7 @@ class images_collections:
     				if file.endswith(".dcm"):
 					paths.append(os.path.join(root, file))
 		return paths
+
 
 #print get_list_files_directory("/home/ohje/Desktop/LIDC-IDRI/")
 m = images_collections("/home/ohje/Desktop/LIDC-IDRI/")
