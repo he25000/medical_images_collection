6a51c4249347bce75344b562943134c214225f74
he25000 <47164918+he25000@users.noreply.github.com>
2019-01-29 21:33:18 -0500

    Add files via upload

---
+++ preprocess_images/list_directory.py
@@ -0,0 +1,19 @@
+
+import os
+class images_collections:
+	paths = []
+	def __init__(self,d_path):
+		self.path=d_path
+
+	def get_list_files_directory(self, dir_path):
+		paths=[]
+		for root, dirs, files in os.walk(dir_path):
+			for file in files:
+    				if file.endswith(".dcm"):
+					paths.append(os.path.join(root, file))
+		return paths
+
+#print get_list_files_directory("/home/ohje/Desktop/LIDC-IDRI/")
+m = images_collections("/home/ohje/Desktop/LIDC-IDRI/")
+print m.get_list_files_directory("/home/ohje/Desktop/LIDC-IDRI/")
+
